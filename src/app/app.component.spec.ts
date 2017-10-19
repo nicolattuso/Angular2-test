827c120751b111dcc6b2839f38208695370ecce0
Nicolas Jean <nicojean23@gmail.com>
2017-10-19 14:58:55 +0200

    Add application

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,56 @@
+import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+@Component({
+  template: ''
+})
+class DummyComponent {
+}
+
+describe('AppComponent', function () {
+  let de: DebugElement;
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let heroServiceStub: any;
+
+  beforeEach(async(() => {
+    heroServiceStub = {
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent, DummyComponent],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'dashboard', component: DummyComponent },
+          { path: 'heroes', component: DummyComponent }
+         ])
+      ],
+      providers: [
+        {provide: HeroService, useValue: heroServiceStub}
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h1'));
+  });
+
+  it('should create component', () => expect(comp).toBeDefined() );
+
+  it('should have expected <h1> text', () => {
+    fixture.detectChanges();
+    const h1 = de.nativeElement;
+    expect(h1.innerText).toMatch(comp.title,
+      '<h1> should contain the title');
+  });
+});
